90330850a11e195a043c7558a18e575e38c0f177
dhcrain <dhcrain@gmail.com>
2016-05-29 14:19:40 -0400

    Working game, needs cleaning up

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 from my_player import Player
 from my_player import Dealer
 from my_player import User
+import os
+import time
 
 
 class Game:
@@ -8,52 +10,59 @@ class Game:
     def __init__(self):
         self.dealer_hand_value = Dealer().hand_value()
         self.user_hand_value = User().hand_value()
-        self.blackjack = False
+
+    def play_blackjack(self):
+        dealer = Dealer()
+        user = User()
+        os.system("clear")
+        print("\n    -*-*- Blackjack -*-*-\n\n"
+              "Closest to 21 without going over, \n"
+              "Dealer stands on 17\n")
+        print("Dealer: ", end="")
+        dealer.deal_cards()
+        print(" ".join(dealer.hand))
+        print("\nYour Cards")
+        user.deal_cards()
+        # print(User.hand_value())
+        print(" ".join(user.hand))
+        user.user_draw()
+        user.hand_value()
+        dealer.dealer_draw()
+        print("\nDealer has {}, and you have {}.\n".format(dealer.hand_value(), user.hand_value()))
+        game.winner()
 
     def winner(self):
+        self.you_win = " *** You Win! ***\n"
+        self.dealer_win = " * Dealer Wins! *\n"
         if user.bust and dealer.bust:
-            print("6 No winner")
+            print("Both busted, Dealer wins")
         elif dealer.hand_value() > user.hand_value():
             if dealer.bust and not user.bust:
-                print("1 You Win!")
-                exit()
-            print("2 Dealer Wins!")
-            exit()  # play again
+                print(self.you_win)
+                game.play_again()
+            print(self.dealer_win)
+            game.play_again()
         elif dealer.hand_value() < user.hand_value():
             if user.bust:
-                print("3 Dealer Wins!")
-                exit()
-            print("4You Win!")
-            exit()
-        elif dealer.hand_value() == 21 and user.hand_value() == 21:
-            print("5 Tie, Dealer Wins!")
-            exit()
+                print(self.dealer_win)
+                game.play_again()
+            print(self.you_win)
+        elif dealer.win and user.win or dealer.hand_value() == user.hand_value():
+            print(" * Both have Blackjack, Dealer Wins! *")
+        elif dealer.win and not user.win:
+            print(" * Dealer Wins! *")
         else:
-            print("7 ELSE Both busted, sad")
-            exit()
-"""
-    def win_bust(self):
-        if Player().hand_value() > 21:
-            print("\n ** Bust! ** ")
-        elif Player().hand_value() == 21:
-            print("\n *** BlackJack! *** ")
-            self.blackjack = True
+            print("That was an odd hand!?!?")
+        game.play_again()
+
+    def play_again(self):
+        if input("\nPlay Again? Y/n ").lower() != "n":
+            print("Shuffling........")
+            time.sleep(.5)
+            game.play_blackjack()
         else:
-            pass
-"""
+            print("\nThanks for playing Blackjack")
+            exit()
 
-
-dealer = Dealer()
-user = User()
 game = Game()
-
-
-user.deal_cards()
-user.user_draw()
-user.hand_value()
-# game.winner()
-
-dealer.dealer_draw()
-# dealer.hand_value()
-
-game.winner()
+game.play_blackjack()
--- my_player.py
+++ my_player.py
@@ -1,5 +1,5 @@
 from my_deck import Deck
-import os
+import time
 
 class Player:
 
@@ -16,7 +16,10 @@ class Player:
             if card[0] in "1JQK":
                 self.hand_total += 10
             elif card[0] is "A":
-                self.hand_total += 11  # OR 1, somehow...
+                if self.hand_total + 11 <= 21:
+                    self.hand_total += 11
+                elif self.hand_total > 21:
+                    self.hand_total += 1
             else:
                 self.hand_total += int(card[0])
         return int(self.hand_total)
@@ -28,8 +31,8 @@ class Player:
         elif self.hand_value() == 21:
             print("\n *** Blackjack! *** \n")
             self.win = True
-        else:
-            print("Else: win_bust")
+        # else:
+        #     print("Else: win_bust")
 
     def deal_cards(self):
         self.hand = [self.game_deck.pop() for _ in range(2)]
@@ -51,13 +54,16 @@ class Player:
 class Dealer(Player):
 
     def dealer_draw(self):
-        print("Dealer: ")
+        # print("\nDealer Draws: ", end="")
+        # super().deal_cards()
+        # print(self.hand)
         # print(self.hand)
         # print(super().hand_value())
         while super().hand_value() <= 17:
             super().draw()
-            print(self.hand)
-        print(super().hand_value())
+            time.sleep(.1)
+            print("Dealer Draws: ", " ".join(self.hand))
+        # print(super().hand_value())
         super().win_bust()
         return super().hand_value()
 
@@ -67,18 +73,24 @@ class Dealer(Player):
 
 class User(Player):
 
+    # def __init__(self):
+    #     super().__init__()
+
     def user_draw(self):
         # os.system("clear")
-        print("User: ")
-        print(self.hand)
-        print(super().hand_value())
+        # super().deal_cards()
+        # print("User: ")
+        # print(self.hand)
+        # print(super().hand_value())
         while super().hand_value() < 21:
-            if input("Draw? Y/n ").lower() != "n":
+            if input("Hit or Stand? H/s ").lower() != "s":
+                print("Your new cards: ", end="")
                 super().draw()
             else:
+                print(" ")
                 break
-            print(self.hand)
-            print(super().hand_value())
+            print(" ".join(self.hand))
+            # print(super().hand_value())
         super().win_bust()
         return super().hand_value()
 
