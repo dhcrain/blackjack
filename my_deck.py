80140faa517536edd457e032f5c2ebf9d9dd26a2
dhcrain <dhcrain@gmail.com>
2016-05-30 22:34:18 -0400

    With help from Brother in Law

--- my_deck.py
+++ my_deck.py
@@ -7,7 +7,8 @@ class Deck:
         self.card_numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
         self.card_suit = ["♥", "♦", "♠", "♣"]
         self.deck = [(num + " " + suit) for num in self.card_numbers for suit in self.card_suit]
-        random.shuffle(self.deck)
+        self.deck *= 6      # 6 cards in the shoe
 
-# deck = Deck()
-# print(deck.deck)
+    def shuffle_deck(self):
+        random.shuffle(self.deck)
+        return self.deck
--- my_player.py
+++ my_player.py
@@ -1,5 +1,3 @@
-from my_deck import Deck
-import time
 
 
 class Player:
@@ -7,69 +5,34 @@ class Player:
     def __init__(self):
         self.hand = []
         self.hand_total = 0
-        self.game_deck = Deck().deck
         self.win = False
+        self.money = 100
 
-    # def ace_reorder(self):  # Cant get this to work as method/function
-    #     for index, card in enumerate(self.hand):
-    #         if card[0] is "A":
-    #             self.hand += [self.hand.pop(index)]
-    #             return self.hand
+    def clear_hand(self):
+        self.hand = []
 
-    def hand_value(self):
-        self.hand_total = 0
-        for card in self.hand:
-            if card[0] in "1JQK":
-                self.hand_total += 10
-            elif card[0] is "A":
-                if self.hand_total + 11 <= 21:
-                    self.hand_total += 11
-                elif self.hand_total > 21:
-                    self.hand_total += 1
-            else:
-                self.hand_total += int(card[0])
-        return int(self.hand_total)
+    def add_card_to_hand(self, card):
+        self.hand.append(card)
 
-    def win_bust(self):
-        if self.hand_value() >= 22:
-            print("\n ** Bust! ** \n")
-        elif self.hand_value() == 21:
-            print("\n *** Blackjack! *** \n")
-            self.win = True
+    def show_hand(self):
+        return self.hand
 
-    def deal_cards(self):
-        self.hand = [self.game_deck.pop() for _ in range(2)]
+    def give_money(self, value):
+        self.money -= value
 
-    def draw(self):
-        self.hand.append(self.game_deck.pop())
-        # sends Aces to the back of the hand list
-        # so that the value can be better calculated
-        for index, card in enumerate(self.hand):
-            if card[0] is "A":
-                self.hand += [self.hand.pop(index)]
+    def get_money(self, value):
+        self.money += value
 
 
 class Dealer(Player):
 
-    def dealer_draw(self):
-        while super().hand_value() <= 17:
-            super().draw()
-            time.sleep(.25)
-            print("Dealer Draws: ", " | ".join(self.hand))
-        super().win_bust()
-        return super().hand_value()
-
+    pass
 
 class User(Player):
 
     def user_draw(self):
-        while super().hand_value() < 21:
-            if input("Hit or Stand? H/s ").lower() != "s":
-                print("Your new cards: ", end="")
-                super().draw()
-            else:
-                print(" ")
-                break
-            print(" | ".join(self.hand))
-        super().win_bust()
-        return super().hand_value()
+        if input("Hit or Stand? H/s ").lower() != "s":
+            return True
+        else:
+            print(" ")
+            return False
